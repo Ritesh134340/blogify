61864ec82013813f7d267674b1d99ccaa82699b2
Ritesh134340 <riteshkumar134340@gmail.com>
2023-05-06 18:41:42 +0530

    import fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Navbar from "../components/nabar";
+import Footer from "../components/footer";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react"
 
