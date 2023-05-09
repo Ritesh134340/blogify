757ceff4cd44444a66fcd6d79c3a73cb043f2677
Ritesh134340 <riteshkumar134340@gmail.com>
2023-05-09 18:11:36 +0530

    about added

--- components/loading.jsx
+++ components/loading.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import styles from  "@/styles/loading.module.css"
 
 const 
 Loading = () => {
   return (
-    <div style={{position:"fixed",top:"0",left:"0",height:"100vh",width:"100%",backgroundColor:"rgba(0,0,0,0.5)",display:"flex",alignItems:"center",justifyContent:"center",zIndex:"1000",height:"100vh",width:"100vw"}}>
-       <img style={{display:'block',width:"50px",height:"50px"}} src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif" alt="loader"/>
+    <div>
+    <div className="fixed left-0 top-0 h-[99vh] w-[100%] flex items-center justify-center bg-[rgba(0,0,0,0.5)] z-[11]">
+    <div className={styles.lds}></div>
     </div>
+   
+    </div>
+ 
+
   )
 }
+
+
+
 
 export default Loading
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Head from "next/head";
+import Loading from "@/components/loading";
+import Image from "next/image";
 
 const About = () => {
   return (
@@ -7,37 +9,24 @@ const About = () => {
       <Head>
         <title>About | Blogify.today</title>
       </Head>
-     
-      <div className="w-95%  mx-auto mt-[130px] mb-[50px] px-4 md:w-[85%] md:mb-[50px]  md:px-6 lg:px-8">
-        <h1 className="text-xl md:text-2xl mb-[30px] lg:text-3xl font-bold mt-8 ">
-          About Blogify.today
-        </h1>
-        <div className="flex flex-col md:flex-row justify-between items-start">
-          <div className="flex-1  md:mr-8">
-            <p className="text-lg md:text-xl leading-relaxed mb-4">
-              Blogify.today is a website where you can read different blogs
-              related to Next.js, React.js, HTML, CSS, and other web development
-              topics.
-            </p>
-            <p className="text-lg md:text-xl leading-relaxed mb-4">
-              Our mission is to provide high-quality content that helps
-              developers improve their skills and stay up-to-date with the
-              latest web development trends.
-            </p>
-            <p className="text-lg md:text-xl leading-relaxed mb-4">
-              We believe that the best way to learn is by doing, so we encourage
-              our readers to try out the examples and tutorials on their own and
-              experiment with new technologies and techniques.
-            </p>
-            <p className="text-lg md:text-xl leading-relaxed">
-              In addition to blog posts, we also offer a variety of resources
-              for web developers, including video tutorials, podcasts, and
-              community forums where you can connect with other developers and
-              share your knowledge and experiences.
-            </p>
-          </div>
-        </div>
-        <div className="mt-8">
+      <h1 className="text-xl text-center md:text-2xl mt-[80px] pt-[25px] pb-[15px] lg:text-3xl font-bold bg-[rgb(243,233,233)]">
+        About Blogify.today
+      </h1>
+
+      <div className="bg-[rgb(243,233,233)] flex flex-col-reverse lg:flex-row lg:items-center">
+        <div className="w-[100%] px-[15px] pb-[25px] md:px-[20px] lg:w-[40%] lg:px-[30px]">
+          <p className="text-lg md:text-xl leading-relaxed mb-4">
+            Blogify.today is a website where you can read different blogs
+            related to Next.js, React.js, HTML, CSS, and other web development
+            topics.
+          </p>
+
+          <p className="text-lg md:text-xl leading-relaxed mb-4">
+            Our mission is to provide high-quality content that helps developers
+            improve their skills and stay up-to-date with the latest web
+            development trends.
+          </p>
+
           <p className="text-lg md:text-xl leading-relaxed mb-4">
             Blogify.today was founded in 2023 by a team of experienced web
             developers who wanted to create a platform for sharing their
@@ -45,10 +34,20 @@ const About = () => {
             passionate about web development and we&apos;re committed to helping
             others learn and grow in their careers.
           </p>
-          <p className="text-lg md:text-xl leading-relaxed">
+
+          <p className="text-lg md:text-xl leading-relaxed mb-4">
             If you have any questions, comments, or feedback, please don&apos;st
             hesitate to us. We&apos;sd love to hear from you!
           </p>
+        </div>
+        <div className="flex-1">
+          <Image
+            className="w-[100%]"
+            src="/assets/about_illus.svg"
+            width={400}
+            height={400}
+            alt="illustration"
+          ></Image>
         </div>
       </div>
     </>
